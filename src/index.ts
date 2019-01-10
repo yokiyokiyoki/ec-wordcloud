af5c34b9845f77f938bcfbfe387abf0b332d3fe5
youjie <youjie@hudongpai.com>
2019-01-10 21:30:05 +0800

    wip(词云图): 初步编写

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,14 @@ const myLib = (text: string): string => {
   return text;
 };
 
+class EWordcloud {
+  constructor(el: HTMLElement) {
+    this.$el = el;
+  }
+  /**
+   * setOption
+   */
+  public setOption(option) {}
+}
+
 export default myLib;
