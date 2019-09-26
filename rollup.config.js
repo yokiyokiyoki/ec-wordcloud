0ad44cd60b7b8b8db6fcc05575aa6ed533746e68
youjie <youjie@datastory.com.cn>
2019-09-26 11:50:23 +0800

    引入commjs插件，以便可以引入cjs规范的包，然后rollup可以打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: "src/index.ts",
@@ -9,5 +11,5 @@ export default {
       format: "umd"
     }
   ],
-  plugins: [typescript()]
+  plugins: [typescript(),commonjs(),resolve()]
 };
